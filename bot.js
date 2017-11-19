2be48e9e3fc48476c7d0c79121c1701de1da977e
Mohamed Mayla <mohamed.mayla@gmail.com>
2017-11-19 11:36:29 +0200

    Load modules from modules directory

--- bot.js
+++ bot.js
@@ -1,23 +1,25 @@
+/* eslint global-require: "off" */
+/* eslint import/no-dynamic-require: "off" */
 const BootBot = require('bootbot');
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 const env = require('node-env-file');
 
-env(__dirname + '/.env');
+env(path.join(__dirname, '/.env'));
 
 if (!process.env.page_token) {
-    console.log('Error: Specify a Facebook page_token in environment.');
-    process.exit(1);
+  console.error('Error: Specify a Facebook page_token in environment.');
+  process.exit(1);
 }
 
 if (!process.env.verify_token) {
-    console.log('Error: Specify a Facebook verify_token in environment.');
-    process.exit(1);
+  console.error('Error: Specify a Facebook verify_token in environment.');
+  process.exit(1);
 }
 
 if (!process.env.app_secret) {
-    console.log('Error: Specify a Facebook app_secret in environment.');
-    process.exit(1);
+  console.error('Error: Specify a Facebook app_secret in environment.');
+  process.exit(1);
 }
 
 const bot = new BootBot({
@@ -27,8 +29,8 @@ const bot = new BootBot({
 });
 
 // add all modules in modules/
-var normalizedPath = path.join(__dirname, 'modules');
-fs.readdirSync(normalizedPath).forEach(function(file) {
+const normalizedPath = path.join(__dirname, 'modules');
+fs.readdirSync(normalizedPath).forEach((file) => {
   const module = require(path.join(normalizedPath, file));
   bot.module(module);
 });
--- modules/echo.js
+++ modules/echo.js
@@ -1,6 +1,6 @@
 module.exports = (bot) => {
   bot.on('message', (payload, chat, data) => {
-    const text = payload.message.text;
+    const { text } = payload.message;
     if (data.captured) { return; }
     chat.say(`Echo: ${text}`);
   });
